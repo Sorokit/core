cfab6ab36b83a90c4f4f1a4365da253966ce4242
Oluwamorowa <peterfagoroye76@gmail.com>
2026-06-28 20:43:19 +0100

    feat: export calculateFeeTiers and add FeeTiers type for fee estimation

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,9 @@ export type {
   FeeEstimate,
   FeeEstimateInput,
   FeeEstimateOptions,
+  FeeTiers,
 } from "./transaction/estimateFee";
+export { calculateFeeTiers } from "./transaction/estimateFee";
 export { streamTransactions } from "./transaction/streamTransactions";
 export type {
   TransactionPage,
